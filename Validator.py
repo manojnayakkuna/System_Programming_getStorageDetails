c59735bf2cacb2772a84ce58d2059edf3b692c75
Manoj Kumar Nayak <manojnayak.kuna@gmail.com>
2021-11-15 01:45:53 -0500

    updated the jenkinsfile

--- Validator.py
+++ Validator.py
@@ -5,7 +5,7 @@ Created on Fri Nov 12 00:51:03 2021
 @author: manoj
 """
 
-class ParameterFile:
+class Validator:
         
     def validateNull(self, stringValue):
         if stringValue is None or stringValue == '' or len(stringValue.strip()) == 0:
